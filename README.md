e4be06eab8770fb60d6298644bd82a9aa25dc8c4
tharhtetsan <tharhtet1234@gmail.com>
2023-03-06 16:19:33 +0630

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tf_image_classifier
+ 
---
+++ android/app/src/main/kotlin/com/example/tf_image_classifier/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tf_image_classifier
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
